cb79f2bd92428ab4ac5adc2e2c3f51b7e695bea9
iris <429328398@qq.com>
2021-11-09 18:49:17 +0800

    feat: 根据服务器的内容重新构建客户端

--- src/main/Test.java
+++ src/main/Test.java
@@ -1,10 +1,14 @@
 package main;
-import main.pythonServiceClient.*;
+import main.PTServiceClient.*;
+import main.DataServiceClient.*;
 
 public class Test {
     public static void main(String[] args) {
         PTServiceService service = new PTServiceService();
         PTServicePort port = service.getPTServicePort();
         System.out.println(port.trans());
+        DataServiceService dataService = new DataServiceService();
+        DataServicePort dataServicePort = dataService.getDataServicePort();
+        System.out.println(dataServicePort.getAllData());
     }
 }
